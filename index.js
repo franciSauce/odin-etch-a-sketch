0a5c260b2e9390dbe9da2751bce56ec03a50299a
franciSauce <careyfrancisowino@gmail.com>
2024-11-10 15:45:08 +0300

    create the canvas-area functions

--- index.js
+++ index.js
@@ -16,4 +16,77 @@ clearBtn.addEventListener("click", clearGrid);
 createGrid();
 draw();
 
-//
+// Functions of the Canvas-area
+function duplicateGrid() {
+    let grid = userValue.value;
+    copyInput.textContent = "x " + grid;
+}
+
+function enterHint() {
+    promptText.textContent = "Enter a number between 2 and 99.";
+}
+
+function createGrid() {
+    let gridSize = userValue.value;
+    if (gridSize < 0 || gridSize > 99 || isNaN(gridSize)) {
+        promptText.textContent = "Make sure it's a number between 2 and 99!"
+    } else {
+        userValue.value = "";
+        grid.innerHTML = "";
+        promptText.textContent = "";
+        copyInput.textContent = "";
+
+        if (gridSize == 0 || gridSize > 99 || gridSize == "") {
+            gridSize = 16;
+        }
+
+        for (let i = 0; i < gridSize; i++) {
+            let row = document.createElement("div");
+            row.classList.add("row");
+            grid.appendChild("row");
+            
+            for (let j = 0; j < gridSize; j++) {
+                let column = document.createElement("div");
+                column.classList.add("column");
+                row.appendChild(column);
+            }
+        }
+    }
+    // Drawing of the Grid
+    draw();
+}
+
+function draw() {
+    let columns = document.getElementsByClassName("column");
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].addEventListener("mouseover", changeColor);
+    }
+}
+
+function changeColor() {
+    const redRad = document.getElementById("red-clr");
+    const greenRad = document.getElementById("green-clr");
+    const blueRad = document.getElementById("blue-clr");
+    const randomRad = document.getElementById("random");
+    const eraserRad = document.getElementById("eraser");
+
+    if (redRad.checked) {
+        this.backgroundColor = rgb(218, 45, 45);
+    } else if (greenRad.checked) {
+        this.backgroundColor = rgb(0, 128, 0);
+    } else if (blueRad.checked) {
+        this.backgroundColor = rgb(0, 0, 255);
+    } else if (randomRad.checked) {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        this.style.backgroundColor = "#" + randomColor;
+    } else if (eraserRad.checked) {
+        this.style.backgroundColor = "";
+    }
+}
+
+function clearGrid() {
+    let columns = document.getElementsByClassName("column");
+    for (let i = 0; i < columns.length; i++) {
+        columns[i].style.backgroundColor = "";
+    }
+}
